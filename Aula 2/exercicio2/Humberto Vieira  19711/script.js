c98a68994b461528440984907c8035d4da293474
humbertoo98 <sousinha2511@gmail.com>
2020-03-02 17:57:01 +0000

    Aula 2

---
+++ Aula 2/exercicio2/Humberto Vieira  19711/script.js
@@ -0,0 +1,27 @@
+
+var Numbers = [];
+  function myFunction() {
+
+      var doc = document.getElementById("Texto").value;
+      Numbers.push(doc);
+      document.getElementById("myText").innerText = Numbers;
+    console.log(Numbers)
+}
+
+  function GetMax() {
+      console.log(Numbers.length)
+      if(Numbers.length >= 5)
+      {
+          document.getElementById("myText2").innerText = Math.max.apply(Math,Numbers);
+      }else
+          {
+              alert("São necassários 5 registos")
+          }
+  }
+
+function Clean() {
+   Numbers = [];
+    document.getElementById("myText2").innerText = "";
+    document.getElementById("myText").innerText = "";
+
+}
