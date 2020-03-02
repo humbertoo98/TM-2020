5bb81abee5a5f9587dae30dde8c48fef15018c50
humbertoo98 <sousinha2511@gmail.com>
2020-03-02 18:00:15 +0000

    Aula 2 Ex2

--- Aula 2/exercicio2/Humberto Vieira  19711/script.js
+++ Aula 2/exercicio2/Humberto Vieira  19711/script.js
@@ -1,26 +1,24 @@
-
 var Numbers = [];
-  function myFunction() {
 
-      var doc = document.getElementById("Texto").value;
-      Numbers.push(doc);
-      document.getElementById("myText").innerText = Numbers;
+function myFunction() {
+
+    var doc = document.getElementById("Texto").value;
+    Numbers.push(doc);
+    document.getElementById("myText").innerText = Numbers;
     console.log(Numbers)
 }
 
-  function GetMax() {
-      console.log(Numbers.length)
-      if(Numbers.length >= 5)
-      {
-          document.getElementById("myText2").innerText = Math.max.apply(Math,Numbers);
-      }else
-          {
-              alert("São necassários 5 registos")
-          }
-  }
+function GetMax() {
+    console.log(Numbers.length)
+    if (Numbers.length >= 5) {
+        document.getElementById("myText2").innerText = Math.max.apply(Math, Numbers);
+    } else {
+        alert("São necassários 5 registos")
+    }
+}
 
 function Clean() {
-   Numbers = [];
+    Numbers = [];
     document.getElementById("myText2").innerText = "";
     document.getElementById("myText").innerText = "";
 
